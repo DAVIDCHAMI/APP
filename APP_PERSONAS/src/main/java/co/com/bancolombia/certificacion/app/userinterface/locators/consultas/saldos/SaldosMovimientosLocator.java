4730be7268ceec912844c9c8b2500902cc92b7cd
“jham” <jalzate@todo1.net>
2019-08-08 11:35:50 -0500

    Cambios en properties

--- APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/userinterface/locators/consultas/saldos/SaldosMovimientosLocator.java
+++ APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/userinterface/locators/consultas/saldos/SaldosMovimientosLocator.java
@@ -8,32 +8,32 @@ import static co.com.bancolombia.certificacion.app.utilidades.mobileobjectfinder
 public class SaldosMovimientosLocator {
     public static final Locator CUENTA_ESPECIFICA_PRODUCTO_LOCATOR = locator()
             .withAndroid("xpath$//android.view.View[contains(@text,'{0}')]/../..//android.view.View[contains(@text, '{1}')]")
-            .withIos("xpath$");
+            .withIos("xpath$a");
     public static final Locator OPCION_CATEGORIA_PRODUCTOS_LOCATOR = locator()
             .withAndroid("xpath$//android.view.View[contains(@resource-id, 'Expandable')][{0}]/android.view.View/android.view.View/android.view.View/android.view.View[1]")
-            .withIos("xpath$");
+            .withIos("xpath$b");
     public static final Locator OPCION_SELECCIONAR_CATEGORIA_PRODUCTOS_LOCATOR = locator()
             .withAndroid("xpath$//android.view.View[contains(@text, '{0}')]/../..")
-            .withIos("xpath$");
+            .withIos("xpath$c");
     public static final Locator LBL_SIN_MOVIMIENTOS_LOCATOR = locator()
             .withAndroid("xpath$//android.view.View[contains(@text,'{0}')]")
-            .withIos("xpath$");
+            .withIos("xpath$d");
     public static final Locator BTN_DETALLE_PRODUCTO_LOCATOR = locator().withAndroid("xpath$//android.view.View[contains(@text,'Detalle del producto')]").withIos("xpath$n");
     public static final Locator BTN_OCULTAR_BANNER_LOCATOR = locator().withAndroid("xpath$//android.view.View[@resource-id='b5-b1-Content']/android.view.View[2]/android.view.View").withIos("xpath$n");
     public static final Locator CONTENEDOR_INFORMACION_PRODUCTO_LOCATOR = locator().withAndroid("xpath$//android.view.View[contains(@resource-id,'l1-{0}-Screen_Account_Line')]").withIos("xpath$n");
     public static final Locator LBL_TIPO_CUENTA_SALDOS_MOVIMIENTOS_LOCATOR = locator()
             .withAndroid("xpath$//android.view.View[contains(@resource-id, 'l1-{0}-AccountContentContainer')]//android.view.View[1]//android.view.View[2]")
-            .withIos("xpath$");
+            .withIos("xpath$e");
     public static final Locator LBL_NUMERO_CUENTA_SALDOS_MOVIMIENTOS_LOCATOR = locator()
             .withAndroid("xpath$//android.view.View[contains(@resource-id, 'l1-{0}-AccountContentContainer')]/android.view.View[2]")
-            .withIos("xpath$");
+            .withIos("xpath$f");
     public static final Locator LBL_SALDO_DISPONIBLE_SALDOS_MOVIMIENTOS_LOCATOR = locator()
             .withAndroid("xpath$//android.view.View[contains(@resource-id, 'l1-{0}-AccountContentContainer')]//android.view.View[1]//following-sibling::android.view.View[3]")
-            .withIos("xpath$");
-    public static final Locator VER_MOVIMIENTOS_LOCATOR = locator().withAndroid("xpath$//android.view.View[@resource-id='l1-{0}-AccountDataContainer2']").withIos("xpath$");
-    public static final Locator LBL_FECHA_MOVIMIENTO_LOCATOR = locator().withAndroid("xpath$//android.view.View[@resource-id='l1-{0}-b35-Column1']//android.view.View[1]").withIos("xpath$");
-    public static final Locator LBL_DESCRIPCION_MOVIMIENTO_LOCATOR = locator().withAndroid("xpath$//android.view.View[@resource-id='l1-{0}-b35-Column1']//android.view.View[2]").withIos("xpath$");
-    public static final Locator LBL_SALDO_LOCATOR = locator().withAndroid("xpath$//android.view.View[@resource-id='l1-{0}-b35-Column2']//android.view.View[1]").withIos("xpath$");
+            .withIos("xpath$g");
+    public static final Locator VER_MOVIMIENTOS_LOCATOR = locator().withAndroid("xpath$//android.view.View[@resource-id='l1-{0}-AccountDataContainer2']").withIos("xpath$h");
+    public static final Locator LBL_FECHA_MOVIMIENTO_LOCATOR = locator().withAndroid("xpath$//android.view.View[@resource-id='l1-{0}-b35-Column1']//android.view.View[1]").withIos("xpath$i");
+    public static final Locator LBL_DESCRIPCION_MOVIMIENTO_LOCATOR = locator().withAndroid("xpath$//android.view.View[@resource-id='l1-{0}-b35-Column1']//android.view.View[2]").withIos("xpath$j");
+    public static final Locator LBL_SALDO_LOCATOR = locator().withAndroid("xpath$//android.view.View[@resource-id='l1-{0}-b35-Column2']//android.view.View[1]").withIos("xpath$k");
 
     private SaldosMovimientosLocator() {
         throw new IllegalStateException(CLASE_LOCATOR);
