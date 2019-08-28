b8459c8923d78e20c89273e110bcab9112d3422f
Daniel Torres <dtorresb@devco.com.co>
2019-08-28 09:38:25 -0500

    Cambios en los mapeos de inscripción de facturas

--- APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/userinterface/locators/administrarfacturas/InscribirFacturasLocator.java
+++ APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/userinterface/locators/administrarfacturas/InscribirFacturasLocator.java
@@ -6,12 +6,12 @@ import static co.com.bancolombia.certificacion.app.utilidades.constantes.TipoCla
 import static co.com.bancolombia.certificacion.app.utilidades.mobileobjectfinder.Locator.locator;
 
 public class InscribirFacturasLocator {
-    public static final Locator BTN_INCRIBIR_MANUALMENTE_LOCATOR = locator().withAndroid("xpath$//android.view.View[contains(@text,'manualmente')]/../preceding-sibling::android.view.View");
-    public static final Locator TXT_EMPRESA_SERVICIO_LOCATOR = locator().withAndroid("xpath$//android.widget.EditText[@resource-id='Input_ServiceOrCompany']");
-    public static final Locator TXT_EMPRESA_SERVICIO_POP_UP_LOCATOR = locator().withAndroid("xpath$//android.view.View[@resource-id='FilterPopup']/android.view.View[6]/android.view.View[1]");
-    public static final Locator TXT_DESCRIPCION_LOCATOR = locator().withAndroid("xpath$//android.widget.EditText[@resource-id='Input_Description']");
-    public static final Locator TXT_REFERENCIA_LOCATOR = locator().withAndroid("xpath$//android.widget.EditText[@resource-id='Input_Personal_Name']");
-    public static final Locator LBL_INSCRIPCION_EXITOSA_LOCATOR = locator().withAndroid("xpath$//android.view.View[contains(@text,'¡Inscripción exitosa!')]");
+    public static final Locator BTN_INCRIBIR_MANUALMENTE_LOCATOR = locator().withAndroid("xpath$//span[contains(text(),'manualmente')]/../../preceding-sibling::div/div");
+    public static final Locator TXT_EMPRESA_SERVICIO_LOCATOR = locator().withAndroid("xpath$//input[@id='Input_ServiceOrCompany']");
+    public static final Locator TXT_EMPRESA_SERVICIO_POP_UP_LOCATOR = locator().withAndroid("xpath$//div[contains(@id,'Content')]/div/div[2]/div/div[1]");
+    public static final Locator TXT_DESCRIPCION_LOCATOR = locator().withAndroid("xpath$//input[@id='Input_Description']");
+    public static final Locator TXT_REFERENCIA_LOCATOR = locator().withAndroid("xpath$//input[@id='Input_Personal_Name']");
+    public static final Locator LBL_INSCRIPCION_EXITOSA_LOCATOR = locator().withAndroid("xpath$//span[text()='¡Inscripción exitosa!']");
 
     private InscribirFacturasLocator(){
         throw new IllegalStateException(CLASE_LOCATOR);
