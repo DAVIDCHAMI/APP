6159229c83ed66617bdbdca194bd0ae355f6ac94
juferz <jfernandezg@devco.com.co>
2019-08-30 16:12:39 -0500

    mapeo transferencia

--- APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/tasks/transferencia/RealizarTransferencia.java
+++ APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/tasks/transferencia/RealizarTransferencia.java
@@ -8,18 +8,19 @@ import co.com.bancolombia.certificacion.app.models.transaccion.Transferencia;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.DoubleClick;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.Scroll;
 import net.serenitybdd.screenplay.conditions.Check;
-import net.serenitybdd.screenplay.waits.WaitUntil;
 
+import static co.com.bancolombia.certificacion.app.userinterface.locators.GeneralLocator.LNK_SIGUIENTE_LOCATOR;
 import static co.com.bancolombia.certificacion.app.userinterface.pages.GeneralPage.BTN_SIGUIENTE;
-import static co.com.bancolombia.certificacion.app.userinterface.pages.MenuPage.OPT_SUB_MENU;
+import static co.com.bancolombia.certificacion.app.userinterface.pages.GeneralPage.LNK_SIGUIENTE;
 import static co.com.bancolombia.certificacion.app.userinterface.pages.consultas.saldos.SaldosMovimientosPage.CUENTA_ESPECIFICA_PRODUCTO;
 import static co.com.bancolombia.certificacion.app.userinterface.pages.transferencia.TransferenciaPage.*;
 import static co.com.bancolombia.certificacion.app.utilidades.constantes.Constantes.TRANSFERIR_PRODUCTOS_NO_INSCRITOS;
 import static co.com.bancolombia.certificacion.app.utilidades.constantes.Constantes.TRANSFERIR_PRODUCTOS_PROPIOS_INSCRITOS;
-import static co.com.bancolombia.certificacion.app.utilidades.enumeradores.TipoTransaccionEnum.TRANSFERIR_DINERO;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
-import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isPresent;
 
 public class RealizarTransferencia implements Task {
     private Transferencia transferencia;
@@ -31,26 +32,22 @@ public class RealizarTransferencia implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
-                WaitUntil.the(OPT_SUB_MENU.of(TRANSFERIR_DINERO.getSubMenu()), isPresent()),
-                Click.on(OPT_SUB_MENU.of(TRANSFERIR_DINERO.getSubMenu())),
-                RealizarScroll.hastaPosicionDeTarget(CUENTA_ESPECIFICA_PRODUCTO.of(transferencia.getProductoOrigen().getTipo(), transferencia.getProductoOrigen().getNumero())),
+                Scroll.to(CUENTA_ESPECIFICA_PRODUCTO.of(transferencia.getProductoOrigen().getTipo(), transferencia.getProductoOrigen().getNumero())),
                 SeleccionarProducto.desdeSaldosMovimientos(transferencia.getProductoOrigen().getTipo(), transferencia.getProductoOrigen().getNumero(), CUENTA_ESPECIFICA_PRODUCTO),
-                Click.on(TXT_VALOR_TRANSFERENCIA),
-                Click.on(TXT_VALOR_TRANSFERENCIA),
-                Escribir.enCampoTexto(transferencia.getMonto()),
-                Click.on(BTN_SIGUIENTE),
+                Enter.theValue(transferencia.getMonto()).into(TXT_VALOR_TRANSFERENCIA),
+                Click.on(LNK_SIGUIENTE),
                 Check.whether((transferencia.getTipoTransferencia()).contains(TRANSFERIR_PRODUCTOS_NO_INSCRITOS)).
                         andIfSo(
                                 Click.on(OPT_TIPO_TRANSFERENCIA.of(TRANSFERIR_PRODUCTOS_NO_INSCRITOS)),
                                 Click.on(TXT_NUMERO_CUENTA_DESTINO),
                                 Escribir.enCampoTexto(transferencia.getProductoDestino().getNumero()),
-                                RealizarScroll.hastaPosicionDeTarget(CHK_TIPO_CUENTA.of(transferencia.getProductoDestino().getTipo())),
+                                //RealizarScroll.hastaPosicionDeTarget(CHK_TIPO_CUENTA.of(transferencia.getProductoDestino().getTipo())),
                                 Click.on(CHK_TIPO_CUENTA.of(transferencia.getProductoDestino().getTipo())),
                                 Click.on(BTN_SIGUIENTE)
                         ), Check.whether((transferencia.getTipoTransferencia()).contains(TRANSFERIR_PRODUCTOS_PROPIOS_INSCRITOS)).
                         andIfSo(
                                 Click.on(OPT_TIPO_TRANSFERENCIA.of(TRANSFERIR_PRODUCTOS_PROPIOS_INSCRITOS)),
-                                RealizarScroll.hastaPosicionDeTarget(CUENTA_ESPECIFICA_PRODUCTO.of(transferencia.getProductoDestino().getTipo(), transferencia.getProductoDestino().getNumero())),
+                               Scroll.to(CUENTA_ESPECIFICA_PRODUCTO.of(transferencia.getProductoDestino().getTipo(), transferencia.getProductoDestino().getNumero())),
                                 SeleccionarProducto.desdeSaldosMovimientos(transferencia.getProductoDestino().getTipo(), transferencia.getProductoDestino().getNumero(), CUENTA_ESPECIFICA_PRODUCTO)
                         ),
 
--- APP_PERSONAS/src/test/resources/features/transferencia/transferencia.feature
+++ APP_PERSONAS/src/test/resources/features/transferencia/transferencia.feature
@@ -6,14 +6,15 @@ Característica: Transferencia desde la app
   Desde mi app
 
   Esquema del escenario: Transferencia a cuentas no inscritas
-    Dado que el Actor carga los datos para la prueba
-      | <ID> | <orientacion> | <codigoError> | <codigoTransaccion> | <resultadoEsperado> | <tipoDocumento> | <numeroDocumento> | <segundaClave> | <primeraPregunta> | <validarClave> |
-    Cuando quiero TRANSFERIR_DINERO del usuario <usuario> con clave <clave>
-    Y diligencios los datos de la transferencia con información
+    Dado que el Actor se autentica en la app
+      | ID   | numeroDocumento   | tipoDocumento   | usuario   | clave   | segundaClave   | orientacion   | codigoError   | codigoTransaccion   | resultadoEsperado   | primeraPregunta   | validarClave   |
+      | <ID> | <numeroDocumento> | <tipoDocumento> | <usuario> | <clave> | <segundaClave> | <orientacion> | <codigoError> | <codigoTransaccion> | <resultadoEsperado> | <primeraPregunta> | <validarClave> |
+    Y el actor quiere TRANSFERIR_DINERO
+    Cuando diligencios los datos de la transferencia con información
       | tipoTransferencia   | numeroCuenta     | tipoCuenta           | monto   | productoDestino   | tipoProductoDestino   |
       | <tipoTransferencia> | <productoOrigen> | <tipoProductoOrigen> | <monto> | <productoDestino> | <tipoProductoDestino> |
 
     Ejemplos:
       | ID | numeroDocumento | tipoDocumento        | usuario   | clave | segundaClave | orientacion | codigoError | codigoTransaccion | resultadoEsperado | primeraPregunta | validarClave | productoOrigen | tipoProductoOrigen | monto  | tipoTransferencia                  | productoDestino | tipoProductoDestino |
    ##@externaldata@./src/test/resources/datadriven/transferencia/Transferencia.xlsx@Datos@2
-   |2   |1010111   |Cédula de ciudadanía   |usercdt66   |1234   |4321   |Alterno   |000   |0369   |NO ERROR   |bolp   |ACTIVO   |406-130790-03   |Corriente   |100000   |Productos propios e inscritos Bancolombia   |406-730790-05   |Cuenta de Ahorro|
+   |2   |1010111   |Cédula de ciudadanía   |usercdt66   |1234   |4321   |Alterno   |000   |0369   |NO ERROR   |bolp   |ACTIVO   |406-130790-03   |Corriente   |100000   |Productos propios e inscritos Bancolombia   |406-732280-07   |Ahorros|
