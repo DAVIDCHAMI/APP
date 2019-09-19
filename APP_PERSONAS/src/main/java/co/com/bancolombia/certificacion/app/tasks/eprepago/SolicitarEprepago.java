5b42f47bb61d6700873c26a376815f1f0aed1722
ingjuanfg <jfernandezg@devco.com.co>
2019-09-19 16:57:18 -0500

    Se refactoriza la tarea SolicitudEprepago, se refactoriza el feature solicitar_eprepago

--- APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/tasks/eprepago/SolicitarEprepago.java
+++ APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/tasks/eprepago/SolicitarEprepago.java
@@ -2,20 +2,19 @@ package co.com.bancolombia.certificacion.app.tasks.eprepago;
 
 import co.com.bancolombia.certificacion.app.interactions.comunes.Escribir;
 import co.com.bancolombia.certificacion.app.interactions.comunes.Saltar;
-
-import static co.com.bancolombia.certificacion.app.userinterface.pages.GeneralPage.BTN_SIGUIENTE;
-import static co.com.bancolombia.certificacion.app.userinterface.pages.GeneralPage.LNK_SIGUIENTE;
-import static co.com.bancolombia.certificacion.app.userinterface.pages.autenticacion.InicioSesionPage.TXT_CLAVE_DIGITOS;
-
+import co.com.bancolombia.certificacion.app.models.builders.UsuarioBuilder;
 import co.com.bancolombia.certificacion.app.models.transaccion.ConfiguracionTransaccion;
+import co.com.bancolombia.certificacion.app.models.usuario.Usuario;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 import net.serenitybdd.screenplay.conditions.Check;
+import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
 import net.serenitybdd.screenplay.waits.WaitUntil;
-import org.apache.poi.util.SystemOutLogger;
 
+import static co.com.bancolombia.certificacion.app.userinterface.pages.GeneralPage.LNK_SIGUIENTE;
+import static co.com.bancolombia.certificacion.app.userinterface.pages.autenticacion.InicioSesionPage.TXT_CLAVE_DIGITOS;
 import static co.com.bancolombia.certificacion.app.userinterface.pages.eprepago.SolicitudEprepagoPage.BTN_SOLICITAR_EPREPAGO;
 import static co.com.bancolombia.certificacion.app.userinterface.pages.eprepago.SolicitudEprepagoPage.CHK_TYC;
 import static co.com.bancolombia.certificacion.app.userinterface.pages.registro.InscripcionClaveDinamicaPage.*;
@@ -23,15 +22,19 @@ import static co.com.bancolombia.certificacion.app.utilidades.constantes.ModeloC
 import static co.com.bancolombia.certificacion.app.utilidades.constantes.VariablesSesionConstantes.TIPO_CORREO_LABORAL;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isEnabled;
-import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isSelected;
 
 public class SolicitarEprepago implements Task {
-    public SolicitarEprepago(){}
+
+    private Usuario usuario1;
+
+    public SolicitarEprepago(Usuario usuario1){this.usuario1 = usuario1;}
 
     @Override
     public <T extends Actor> void performAs(T actor) {
         ConfiguracionTransaccion usuario = actor.recall(MODELO_DATOS_TRANSACCION);
+
         actor.attemptsTo(
+                WaitUntil.the(CHK_TYC, WebElementStateMatchers.isEnabled()),
                 Click.on(CHK_TYC),
                 Click.on(BTN_SOLICITAR_EPREPAGO),
                 Saltar.onBoarding(),
@@ -41,15 +44,19 @@ public class SolicitarEprepago implements Task {
                 Click.on(BTN_CONTINUAR_SEGUNDA_CLAVE),
                 Enter.theValue(usuario.getUsuario().getNombreUsuario()).into(TXT_NOMBRE_PERSONALIZADO),
                 Click.on(LNK_SIGUIENTE),
-                Enter.theValue(usuario.getUsuario().getCorreo()).into(TXT_CORREO)
-                /*Check.whether(TIPO_CORREO_LABORAL.equalsIgnoreCase(usuario.getUsuario().getTipoCorreo())).
-                        otherwise(WaitUntil.the(CHK_TIPO_CORREO.of(usuario.getUsuario().getTipoCorreo()), isEnabled()),
-                                Click.on(CHK_TIPO_CORREO.of(usuario.getUsuario().getTipoCorreo())))*/
-
+                Enter.theValue(usuario1.getCorreo()).into(TXT_CORREO),
+                Check.whether(TIPO_CORREO_LABORAL.equalsIgnoreCase(usuario1.getTipoCorreo())).
+                        otherwise(WaitUntil.the(CHK_TIPO_CORREO.of(usuario1.getTipoCorreo()), isEnabled()),
+                                Click.on(CHK_TIPO_CORREO.of(usuario1.getTipoCorreo()))),
+                Click.on(LNK_SIGUIENTE),
+                Enter.theValue(usuario1.getNumeroCelular()).into(TXT_NUMERO_CELULAR),
+                Click.on(CHK_ACEPTO_TERMINOS),
+                Click.on(BTN_INSCRIBIR_CLAVE)
         );
+
     };
 
-    public static SolicitarEprepago enLaAppBancolombia(){
-        return instrumented(SolicitarEprepago.class);
+    public static SolicitarEprepago enLaAppBancolombia(UsuarioBuilder usuarioBuilder){
+        return instrumented(SolicitarEprepago.class, usuarioBuilder.build());
     }
 }
