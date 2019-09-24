fc82a99dcae5c7248a72497a43e47f8da207ae20
jrua01 <jrua@todo1.net>
2019-09-24 16:20:58 -0500

    refactorizacion

--- APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/tasks/billetera/RegistrarBilletera.java
+++ APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/tasks/billetera/RegistrarBilletera.java
@@ -1,24 +1,24 @@
 package co.com.bancolombia.certificacion.app.tasks.billetera;
 
 import co.com.bancolombia.certificacion.app.interactions.billetera.AceptarTerminosCondiciones;
-import co.com.bancolombia.certificacion.app.interactions.comunes.Esperar;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.waits.WaitUntil;
 
-import static co.com.bancolombia.certificacion.app.userinterface.pages.GeneralPage.*;
+import static co.com.bancolombia.certificacion.app.userinterface.pages.GeneralPage.BTN_PERMISO;
+import static co.com.bancolombia.certificacion.app.userinterface.pages.GeneralPage.LNK_SALTAR;
 import static co.com.bancolombia.certificacion.app.userinterface.pages.billetera.BilleteraPage.*;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
-import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.*;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isEnabled;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class RegistrarBilletera implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
-                Esperar.unTiempo(20000),
                 WaitUntil.the(BTN_CONTINUAR_BILLETERA, isEnabled()),
                 Click.on(BTN_CONTINUAR_BILLETERA),
                 WaitUntil.the(BTN_PERMISO, isEnabled()),
