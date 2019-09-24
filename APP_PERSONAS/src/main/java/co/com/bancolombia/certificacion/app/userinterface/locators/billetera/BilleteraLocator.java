6b651a4fc2277fc3aa1cbcbe4aa8275f5bcd5464
jrua01 <jrua@todo1.net>
2019-09-24 16:40:54 -0500

    se refactoriza clase BilleteraLocator

--- APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/userinterface/locators/billetera/BilleteraLocator.java
+++ APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/userinterface/locators/billetera/BilleteraLocator.java
@@ -7,7 +7,7 @@ import static co.com.bancolombia.certificacion.app.utilidades.mobileobjectfinder
 
 public class BilleteraLocator {
     public static final Locator BTN_CONTINUAR_BILLETERA_LOCATOR = locator().withAndroid("xpath$//button[text()='CONTINUAR']").withIos("xpath$d");
-    public static final Locator BTN_SIGUIENTE_ON_BOARDING_LOCATOR = locator().withAndroid("xpath$//button[text()='SIGUIENTE']]").withIos("xpath$d");
+    public static final Locator BTN_SIGUIENTE_ON_BOARDING_LOCATOR = locator().withAndroid("xpath$//button[text()='SIGUIENTE']").withIos("xpath$d");
     public static final Locator CHK_TERMINOS_CONDICIONES_BILLETERA_LOCATOR = locator().withAndroid("xpath$//input[@id='Chk_TerminosCondiciones']").withIos("xpath$d");
     public static final Locator BTN_COMENZAR_REGISTRO_BILLETERA_LOCATOR = locator().withAndroid("xpath$//button//span[text()='COMENZAR']").withIos("xpath$d");
     public static final Locator LBL_TARJETAS_LISTADAS_LOCATOR = locator().withAndroid("xpath$//div[@class='content']").withIos("xpath$d");
