881ef869852976e8872531b0dae5255115696e69
jrua01 <jrua@todo1.net>
2019-10-04 16:17:15 -0500

    Se crea la tarea DescargarEprepago

--- APP_PERSONAS/src/test/java/co/com/bancolombia/certificacion/app/stepdefinitions/eprepago/DescargarTarjetaVirtualEprepagoStepDefinition.java
+++ APP_PERSONAS/src/test/java/co/com/bancolombia/certificacion/app/stepdefinitions/eprepago/DescargarTarjetaVirtualEprepagoStepDefinition.java
@@ -1,14 +1,19 @@
 package co.com.bancolombia.certificacion.app.stepdefinitions.eprepago;
 
+import co.com.bancolombia.certificacion.app.tasks.eprepago.DescargarEprepago;
 import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Entonces;
+
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class DescargarTarjetaVirtualEprepagoStepDefinition {
 
     @Cuando("^quiere descargar una tarjeta ePrepago activa$")
     public void descargarTarjetaEPrepagoActiva() {
+        theActorInTheSpotlight().attemptsTo(
+                DescargarEprepago.enLaAppBancolombia()
+        );
     }
-
 
     @Entonces("^el deberia de ver un mensaje descarga ePrepago exitosa$")
     public void deberiaDeVerMensajeDescargaEPrepagoExitosa() {
