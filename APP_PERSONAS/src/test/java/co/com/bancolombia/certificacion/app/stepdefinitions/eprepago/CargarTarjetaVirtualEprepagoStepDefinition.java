9df5f31b1d8a945f0935c1b1269c20c070acfba0
jrua01 <jrua@todo1.net>
2019-10-03 20:33:26 -0500

    Se agrega el parametro montoRecarga a la clase RecargarTarjetaVirtualEprepagoStepDefinition

--- APP_PERSONAS/src/test/java/co/com/bancolombia/certificacion/app/stepdefinitions/eprepago/CargarTarjetaVirtualEprepagoStepDefinition.java
+++ APP_PERSONAS/src/test/java/co/com/bancolombia/certificacion/app/stepdefinitions/eprepago/CargarTarjetaVirtualEprepagoStepDefinition.java
@@ -17,5 +17,6 @@ public class CargarTarjetaVirtualEprepagoStepDefinition {
 
     @Entonces("^el deberia de ver un mensaje de carga de tarjeta ePrepago exitosa$")
     public void deberiaVerMensajeCargaTarjetaEprepagoExitosa() {
+        
     }
 }
