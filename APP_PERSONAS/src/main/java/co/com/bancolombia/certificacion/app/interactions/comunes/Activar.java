1883bbd876ae95d485aa31efb74353daa9e9d94c
jrua01 <jrua@todo1.net>
2019-10-07 11:02:57 -0500

    Se crea la interácción para cancelar la verificación por huella en la app

--- APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/interactions/comunes/Activar.java
+++ APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/interactions/comunes/Activar.java
@@ -2,14 +2,22 @@ package co.com.bancolombia.certificacion.app.interactions.comunes;
 
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Interaction;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 
+import static co.com.bancolombia.certificacion.app.userinterface.pages.GeneralPage.BTN_SI;
+import static co.com.bancolombia.certificacion.app.userinterface.pages.GeneralPage.LNK_CANCELAR;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isEnabled;
 
 public class Activar implements Interaction {
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
-                
+                WaitUntil.the(LNK_CANCELAR, isEnabled()),
+                Click.on(LNK_CANCELAR),
+                WaitUntil.the(BTN_SI, isEnabled()),
+                Click.on(BTN_SI)
         );
     }
 
