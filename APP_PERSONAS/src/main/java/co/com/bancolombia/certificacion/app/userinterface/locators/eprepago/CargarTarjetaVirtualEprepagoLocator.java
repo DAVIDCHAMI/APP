f3e56d0af6b21c52058bb4ba3d4befb6147ea874
jrua01 <jrua@todo1.net>
2019-10-07 20:17:29 -0500

    Eliminar saltos de linea

--- APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/userinterface/locators/eprepago/CargarTarjetaVirtualEprepagoLocator.java
+++ APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/userinterface/locators/eprepago/CargarTarjetaVirtualEprepagoLocator.java
@@ -9,7 +9,6 @@ public class CargarTarjetaVirtualEprepagoLocator {
     public static final Locator BTN_SUB_MENU_EPREPAGO_LOCATOR = locator().withAndroid("xpath$//span[text()='e-Prepago']/../../../following-sibling::div/div").withIos("xpath$q");
     public static final Locator BTN_SELECCIONAR_OPCION_LOCATOR = locator().withAndroid("xpath$//div[@id='EpagoOptions']/..//*[contains(text(),'{0}')]").withIos("xpath$q");
     public static final Locator TXT_VALOR_RECARGA_LOCATOR = locator().withAndroid("xpath$//div[contains(@id,'MaskedContent')]/descendant::input").withIos("xpath$q");
-    public static final Locator BTN_RECARGAR_EPREPAGO_LOCATOR = locator().withAndroid("xpath$//span[text()='RECARGAR']").withIos("xpath$q");
     public static final Locator LNK_RECARGAR_EPREPAGO_LOCATOR = locator().withAndroid("xpath$//span[text()='Recargar']").withIos("xpath$q");
     public static final Locator LBL_RECARGA_EXITOSA_EPREPAGO_LOCATOR = locator().withAndroid("xpath$//span[text()='¡Recarga exitosa!']").withIos("xpath$q");
 
