90b3462176f91d83b638f58833eca5e0fe43dc08
jrua01 <jrua@todo1.net>
2019-10-10 23:01:46 -0500

    Refactorizacion Tarea CargarEprepago

--- APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/tasks/eprepago/CargarEprepago.java
+++ APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/tasks/eprepago/CargarEprepago.java
@@ -1,11 +1,12 @@
 package co.com.bancolombia.certificacion.app.tasks.eprepago;
 
+import co.com.bancolombia.certificacion.app.interactions.comunes.Validar;
 import co.com.bancolombia.certificacion.app.interactions.consultas.saldos.SeleccionarCategoria;
 import co.com.bancolombia.certificacion.app.interactions.eprepago.SeleccionarOpcion;
 import co.com.bancolombia.certificacion.app.interactions.scroll.RealizarScroll;
 import co.com.bancolombia.certificacion.app.models.builders.ProductoBuilder;
 import co.com.bancolombia.certificacion.app.models.productos.Producto;
-import co.com.bancolombia.certificacion.app.utilidades.administradores.Verificar;
+import co.com.bancolombia.certificacion.app.questions.eprepago.Verificar;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
@@ -38,15 +39,16 @@ public class CargarEprepago implements Task {
         actor.attemptsTo(
                 SeleccionarCategoria.deSaldosMovimientos(opcionMenu),
                 SeleccionarOpcion.deSubmenu(RECARGA_EPREPAGO),
-                Check.whether(Verificar.elementoVisible(actor, LBL_PRODUCTO_ORIGEN_EPREPAGO))
-                        .andIfSo(
-                                RealizarScroll.hastaPosicionDeTarget(BTN_PRODUCTO_ORIGEN.of(producto.getTipo(), producto.getNumero())),
-                                Click.on(BTN_PRODUCTO_ORIGEN.of(producto.getTipo(), producto.getNumero()))
-                        ),
+                Check.whether(Verificar.elementoVisible()).andIfSo(
+                        RealizarScroll.hastaTargetVisible(BTN_PRODUCTO_ORIGEN.of(producto.getTipo(), producto.getNumero())),
+                        Click.on(BTN_PRODUCTO_ORIGEN.of(producto.getTipo(), producto.getNumero()))
+                ),
                 Enter.theValue(valorCarga).into(TXT_VALOR_RECARGA_EPREPAGO),
                 WaitUntil.the(LNK_SIGUIENTE, isEnabled()),
                 Click.on(LNK_SIGUIENTE),
-                Click.on(LNK_RECARGAR_EPREPAGO)
+                WaitUntil.the(LNK_RECARGAR_EPREPAGO, isEnabled()),
+                Click.on(LNK_RECARGAR_EPREPAGO),
+                Validar.carga()
         );
 
         actor.remember(MODELO_PRODUCTO, producto);
