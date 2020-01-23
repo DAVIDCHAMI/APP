85abdf4f12b3ec40efd07519a4efc10bc3c3c324
Jham Pier <jalzate@todo1.net>
2020-01-23 14:01:36 -0500

    consulta saldos ecard

--- APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/exceptions/autenticacion/OpcionAunNoEresClienteException.java
+++ APP_PERSONAS/src/main/java/co/com/bancolombia/certificacion/app/exceptions/autenticacion/OpcionAunNoEresClienteException.java
@@ -1,7 +1,6 @@
 package co.com.bancolombia.certificacion.app.exceptions.autenticacion;
 
 public class OpcionAunNoEresClienteException extends AssertionError {
-
     public static final String OPCION_AUN_NO_ERES_CLIENTE_NO_VISIBLE = "Opcion aun no eres cliente no fue visualizada";
 
     public OpcionAunNoEresClienteException(String message, Throwable cause) {
